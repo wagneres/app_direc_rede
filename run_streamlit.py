a9f168dff3b2aacf288497e56a080d6c77af110d
wagneres <wagner2235@icloud.com>
2022-04-18 00:24:29 -0300

    modelo_v1

--- run_streamlit.py
+++ run_streamlit.py
@@ -219,3 +219,11 @@ plt.ylim(int(y_min), int(y_max)+5)
 plt.legend(fontsize=6)
 plt.tick_params(labelsize=6)
 st.pyplot(fig)
+
+
+
+df = pd.DataFrame(
+     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+     columns=['lat', 'lon'])
+
+st.map(df)
